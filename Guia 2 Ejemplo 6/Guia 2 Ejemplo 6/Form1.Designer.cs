65b0971fa919d601dd44186be0cb0035d05d0ea1
Whyzkell <diegov.penado@gmail.com>
2024-07-08 13:46:30 -0600

    ejemplo 6

---
+++ Guia 2 Ejemplo 6/Guia 2 Ejemplo 6/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Guia_2_Ejemplo_6
+{
+    partial class frmReves
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblReves = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtReves = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(106, 85);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(44, 13);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Numero";
+            // 
+            // lblReves
+            // 
+            this.lblReves.AutoSize = true;
+            this.lblReves.Location = new System.Drawing.Point(106, 247);
+            this.lblReves.Name = "lblReves";
+            this.lblReves.Size = new System.Drawing.Size(45, 13);
+            this.lblReves.TabIndex = 1;
+            this.lblReves.Text = "Al reves";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(115, 135);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(423, 20);
+            this.txtNumero.TabIndex = 4;
+            this.txtNumero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumero_KeyPress);
+            // 
+            // txtReves
+            // 
+            this.txtReves.Location = new System.Drawing.Point(109, 300);
+            this.txtReves.Name = "txtReves";
+            this.txtReves.Size = new System.Drawing.Size(429, 20);
+            this.txtReves.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(109, 354);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(160, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(366, 354);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(172, 23);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // frmReves
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtReves);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblReves);
+            this.Controls.Add(this.lblNumero);
+            this.Name = "frmReves";
+            this.Text = "Monstrar un numero al reves";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblReves;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtReves;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Guia 2 Ejemplo 6/Guia 2 Ejemplo 6/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia_2_Ejemplo_6
+{
+    public partial class frmReves : Form
+    {
+        public frmReves()
+        {
+            InitializeComponent();
+        }
+
+        public static bool IsNumeric(string value)
+        {
+            return Regex.IsMatch(value, @"^\d+$");
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            long numero;
+            if (long.TryParse(txtNumero.Text, out numero) && numero > 0 && numero <= 999999)
+            {
+                // ...
+            }
+            else
+            {
+                MessageBox.Show("El dato que ingreso no es un número válido", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtNumero.Clear();
+                txtNumero.BackColor = Color.Red;
+            }
+
+            string numeroStr = numero.ToString();
+            char[] chars = numeroStr.ToCharArray();
+            Array.Reverse(chars);
+            string revesStr = new string(chars);
+            txtReves.Text = revesStr;
+        }
+
+        private void txtNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            txtNumero.BackColor = Color.White;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNumero.Clear();
+            txtReves.Clear();
+        }
+    }
+}
