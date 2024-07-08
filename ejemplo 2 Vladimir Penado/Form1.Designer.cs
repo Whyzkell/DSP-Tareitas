033948a3dec533cd1fa2d4713ff54ba7e84f5bc8
Whyzkell <diegov.penado@gmail.com>
2024-07-07 19:10:49 -0600

    ejemplo2

---
+++ ejemplo 2 Vladimir Penado/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace ejemplo_2_Vladimir_Penado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmpresa = new System.Windows.Forms.TextBox();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.txtTiempo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTasaInterEX = new System.Windows.Forms.TextBox();
+            this.rdbInteres1 = new System.Windows.Forms.RadioButton();
+            this.rdbInteres2 = new System.Windows.Forms.RadioButton();
+            this.rdbInteres3 = new System.Windows.Forms.RadioButton();
+            this.lstResul = new System.Windows.Forms.ListBox();
+            this.btnAnalisis = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre de Empresa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Monto prestamo ($)";
+            // 
+            // txtEmpresa
+            // 
+            this.txtEmpresa.Location = new System.Drawing.Point(180, 40);
+            this.txtEmpresa.Name = "txtEmpresa";
+            this.txtEmpresa.Size = new System.Drawing.Size(100, 20);
+            this.txtEmpresa.TabIndex = 2;
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(180, 110);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(100, 20);
+            this.txtMonto.TabIndex = 3;
+            // 
+            // txtTiempo
+            // 
+            this.txtTiempo.Location = new System.Drawing.Point(619, 117);
+            this.txtTiempo.Name = "txtTiempo";
+            this.txtTiempo.Size = new System.Drawing.Size(100, 20);
+            this.txtTiempo.TabIndex = 4;
+            this.txtTiempo.Text = "2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(522, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tiempo (años)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tasa Interés (%):";
+            // 
+            // txtTasaInterEX
+            // 
+            this.txtTasaInterEX.Location = new System.Drawing.Point(561, 226);
+            this.txtTasaInterEX.Name = "txtTasaInterEX";
+            this.txtTasaInterEX.Size = new System.Drawing.Size(100, 20);
+            this.txtTasaInterEX.TabIndex = 7;
+            this.txtTasaInterEX.Text = "0";
+            // 
+            // rdbInteres1
+            // 
+            this.rdbInteres1.AutoSize = true;
+            this.rdbInteres1.Checked = true;
+            this.rdbInteres1.Location = new System.Drawing.Point(69, 229);
+            this.rdbInteres1.Name = "rdbInteres1";
+            this.rdbInteres1.Size = new System.Drawing.Size(42, 17);
+            this.rdbInteres1.TabIndex = 8;
+            this.rdbInteres1.TabStop = true;
+            this.rdbInteres1.Text = "5 %";
+            this.rdbInteres1.UseVisualStyleBackColor = true;
+            this.rdbInteres1.Click += new System.EventHandler(this.rdbInteres1_Click);
+            // 
+            // rdbInteres2
+            // 
+            this.rdbInteres2.AutoSize = true;
+            this.rdbInteres2.Location = new System.Drawing.Point(195, 229);
+            this.rdbInteres2.Name = "rdbInteres2";
+            this.rdbInteres2.Size = new System.Drawing.Size(51, 17);
+            this.rdbInteres2.TabIndex = 9;
+            this.rdbInteres2.Text = "7.5 %";
+            this.rdbInteres2.UseVisualStyleBackColor = true;
+            this.rdbInteres2.Click += new System.EventHandler(this.rdbInteres2_Click);
+            // 
+            // rdbInteres3
+            // 
+            this.rdbInteres3.AutoSize = true;
+            this.rdbInteres3.Location = new System.Drawing.Point(346, 229);
+            this.rdbInteres3.Name = "rdbInteres3";
+            this.rdbInteres3.Size = new System.Drawing.Size(88, 17);
+            this.rdbInteres3.TabIndex = 10;
+            this.rdbInteres3.Text = "Otro, indique:";
+            this.rdbInteres3.UseVisualStyleBackColor = true;
+            this.rdbInteres3.CheckedChanged += new System.EventHandler(this.rdbInteres3_CheckedChanged);
+            // 
+            // lstResul
+            // 
+            this.lstResul.FormattingEnabled = true;
+            this.lstResul.Location = new System.Drawing.Point(69, 276);
+            this.lstResul.Name = "lstResul";
+            this.lstResul.Size = new System.Drawing.Size(592, 82);
+            this.lstResul.TabIndex = 11;
+            // 
+            // btnAnalisis
+            // 
+            this.btnAnalisis.Location = new System.Drawing.Point(93, 405);
+            this.btnAnalisis.Name = "btnAnalisis";
+            this.btnAnalisis.Size = new System.Drawing.Size(132, 23);
+            this.btnAnalisis.TabIndex = 12;
+            this.btnAnalisis.Text = "Evaluar Prestamo";
+            this.btnAnalisis.UseVisualStyleBackColor = true;
+            this.btnAnalisis.Click += new System.EventHandler(this.btnAnalisis_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(334, 405);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(145, 23);
+            this.btnNuevo.TabIndex = 13;
+            this.btnNuevo.Text = "Nuevo Prestamo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(561, 405);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(129, 23);
+            this.btnSalir.TabIndex = 14;
+            this.btnSalir.Text = "Finalizar Programa";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnAnalisis);
+            this.Controls.Add(this.lstResul);
+            this.Controls.Add(this.rdbInteres3);
+            this.Controls.Add(this.rdbInteres2);
+            this.Controls.Add(this.rdbInteres1);
+            this.Controls.Add(this.txtTasaInterEX);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTiempo);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.txtEmpresa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculo del Monto a pagar por un Prestamo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmpresa;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.TextBox txtTiempo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTasaInterEX;
+        private System.Windows.Forms.RadioButton rdbInteres1;
+        private System.Windows.Forms.RadioButton rdbInteres2;
+        private System.Windows.Forms.RadioButton rdbInteres3;
+        private System.Windows.Forms.ListBox lstResul;
+        private System.Windows.Forms.Button btnAnalisis;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ ejemplo 2 Vladimir Penado/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemplo_2_Vladimir_Penado
+{
+    public partial class Form1 : Form
+    {
+
+        double TasaI;
+        public static Boolean EsNumero(string valor)
+        {
+        //retorna true si valor string del parametro representa un valor entero
+            int result;
+            return int.TryParse(valor, out result);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void rdbInteres3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbInteres3.Checked == true)
+            {
+                 txtTasaInterEX.Enabled = true;
+                 txtTasaInterEX.Focus();
+                 }
+             else
+                 {
+                 txtTasaInterEX.Text = "0";
+                 txtTasaInterEX.Enabled = false;
+            }
+        }
+
+        private void rdbInteres1_Click(object sender, EventArgs e)
+        {
+          
+
+            TasaI = 0.05;
+        }
+
+        private void rdbInteres2_Click(object sender, EventArgs e)
+        {
+           
+
+            TasaI = 0.075;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnAnalisis_Click(object sender, EventArgs e)
+        {
+            //Declaracion de variables a utilizar
+            string NomEmpre;
+            // valor de prestamo inicial y valor que pagara al finalizar plazo
+            double MontoInic = 0, MontoFin = 0;
+            int Tiempo; //tiempo en año para pagar prestamo
+            NomEmpre = txtEmpresa.Text; //toma texto escrito en contorl txtEmpresa
+            NomEmpre = NomEmpre.Trim(); //Quita el espacio-blanco al inicio-final
+            if (NomEmpre.Length == 0)
+            { // evalua si ha escrito nombre de empresa
+                MessageBox.Show("Debe indicar Nombre de la empresa", "ERROR",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMonto.Focus(); //metodo para que control txtEmpresa reciba cursor
+                return; //finaliza ejecucion de este procedimiento
+            }
+            if (!(EsNumero(txtMonto.Text)))
+            { //invoca metodo EsNumero
+                MessageBox.Show("Valor Monto incorrecto", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtMonto.Focus(); return; //finaliza ejecucion de este procedimiento
+            }
+            else
+            {
+                MontoInic = Convert. ToDouble(txtMonto.Text);
+                if (!(MontoInic > 0))
+                {
+                    MessageBox.Show("Valor Monto no puede ser negativo", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //metodo que indica que control txtEmpresa recibira cursor txtMonto.Focus();
+                    return;
+                }
+            }
+            //toma tiempo en años dado por usuario
+            Tiempo = Convert.ToInt32(txtTiempo.Text);
+            
+            //si selecciono Tasa interes 3, valida que esta sea correcta
+            txtTasaInterEX.Text = txtTasaInterEX.Text.Trim();
+            if (rdbInteres3.Checked == true)
+                if (txtTasaInterEX.Text.Length > 0)
+                    if (!(EsNumero(txtTasaInterEX.Text) == true))
+                    {
+                        MessageBox.Show("Tasa interes incorrecto", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error); txtTasaInterEX.Text = "0"; txtTasaInterEX.Focus();
+                        return;
+                    }
+                    else
+                        TasaI = Convert.ToDouble(txtTasaInterEX.Text) / 100;
+                else
+                {
+                    MessageBox.Show("Aun no ha indicado una tasa interes", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Information); txtTasaInterEX.Focus();
+                    return;
+                }
+            //Hace el cálculo del monto de prestamo que pagara empresa
+            MontoFin = 1 + TasaI;
+            MontoFin = MontoInic * (Math.Pow(Convert.ToDouble (MontoFin), Tiempo));
+            TasaI *= 100; //En cuadro de lista, muestra la respuesta (Monto a pagar)
+            lstResul.Items.Clear();
+            lstResul.Items.Add("Empresa: " + txtEmpresa.Text);
+            lstResul.Items.Add("Monto: $" + MontoInic + ", Tasa anual: " + TasaI);
+            lstResul.Items.Add("Monto a pagar: $" + MontoFin);
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtEmpresa.Clear();
+            txtMonto.Clear();
+            txtTiempo.Text = "2";
+            rdbInteres1.Checked = true;
+            txtTasaInterEX.Clear();
+            txtTasaInterEX.Enabled = false;
+            lstResul.Items.Clear();
+            txtEmpresa.Focus();
+        }
+    }
+}
