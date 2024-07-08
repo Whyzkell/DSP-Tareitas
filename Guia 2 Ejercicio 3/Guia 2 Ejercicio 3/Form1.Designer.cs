bf37ac2846d949d9b1b28a6dfcc7e903e8b5e4d3
Whyzkell <diegov.penado@gmail.com>
2024-07-08 14:59:49 -0600

    Guia 2 Ejercicio 3

---
+++ Guia 2 Ejercicio 3/Guia 2 Ejercicio 3/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Guia_2_Ejercicio_3
+{
+    partial class wasasin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lblMayorNegativo = new System.Windows.Forms.Label();
+            this.lblPositivos = new System.Windows.Forms.Label();
+            this.lblPromedioNegativos = new System.Windows.Forms.Label();
+            this.lblPorcentaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(118, 77);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 0;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(128, 177);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 1;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // lblMayorNegativo
+            // 
+            this.lblMayorNegativo.AutoSize = true;
+            this.lblMayorNegativo.Location = new System.Drawing.Point(334, 82);
+            this.lblMayorNegativo.Name = "lblMayorNegativo";
+            this.lblMayorNegativo.Size = new System.Drawing.Size(79, 13);
+            this.lblMayorNegativo.TabIndex = 2;
+            this.lblMayorNegativo.Text = "MayorNegativo";
+            // 
+            // lblPositivos
+            // 
+            this.lblPositivos.AutoSize = true;
+            this.lblPositivos.Location = new System.Drawing.Point(334, 144);
+            this.lblPositivos.Name = "lblPositivos";
+            this.lblPositivos.Size = new System.Drawing.Size(49, 13);
+            this.lblPositivos.TabIndex = 3;
+            this.lblPositivos.Text = "Positivos";
+            // 
+            // lblPromedioNegativos
+            // 
+            this.lblPromedioNegativos.AutoSize = true;
+            this.lblPromedioNegativos.Location = new System.Drawing.Point(335, 203);
+            this.lblPromedioNegativos.Name = "lblPromedioNegativos";
+            this.lblPromedioNegativos.Size = new System.Drawing.Size(107, 13);
+            this.lblPromedioNegativos.TabIndex = 4;
+            this.lblPromedioNegativos.Text = "Promedios Negativos";
+            // 
+            // lblPorcentaje
+            // 
+            this.lblPorcentaje.AutoSize = true;
+            this.lblPorcentaje.Location = new System.Drawing.Point(333, 273);
+            this.lblPorcentaje.Name = "lblPorcentaje";
+            this.lblPorcentaje.Size = new System.Drawing.Size(58, 13);
+            this.lblPorcentaje.TabIndex = 5;
+            this.lblPorcentaje.Text = "Porcentaje";
+            // 
+            // wasasin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPorcentaje);
+            this.Controls.Add(this.lblPromedioNegativos);
+            this.Controls.Add(this.lblPositivos);
+            this.Controls.Add(this.lblMayorNegativo);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtNumero);
+            this.Name = "wasasin";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label lblMayorNegativo;
+        private System.Windows.Forms.Label lblPositivos;
+        private System.Windows.Forms.Label lblPromedioNegativos;
+        private System.Windows.Forms.Label lblPorcentaje;
+    }
+}
+
---
+++ Guia 2 Ejercicio 3/Guia 2 Ejercicio 3/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia_2_Ejercicio_3
+{
+    public partial class wasasin : Form
+    {
+        public wasasin()
+        {
+            InitializeComponent();
+        }
+
+        private int[] numeros = new int[0];
+        private int mayorNegativo = -40;
+        private int positivos = 0;
+        private int negativos = 0;
+        private int ceros = 0;
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            int numero;
+            if (int.TryParse(txtNumero.Text, out numero) && numero >= -40 && numero <= 45)
+            {
+                Array.Resize(ref numeros, numeros.Length + 1);
+                numeros[numeros.Length - 1] = numero;
+
+                if (numero < 0)
+                {
+                    if (numero > mayorNegativo)
+                    {
+                        mayorNegativo = numero;
+                    }
+                    negativos++;
+                }
+                else if (numero > 0)
+                {
+                    positivos++;
+                }
+                else
+                {
+                    ceros++;
+                }
+
+                lblMayorNegativo.Text = $"Mayor número negativo: {mayorNegativo}";
+                lblPositivos.Text = $"Números positivos: {positivos}";
+                lblPromedioNegativos.Text = $"Promedio de negativos: {(negativos > 0 ? (double)negativos / numeros.Length : 0):F2}";
+                lblPorcentaje.Text = $"Porcentaje: Positivos {((double)positivos / numeros.Length) * 100:F2}%, Negativos {((double)negativos / numeros.Length) * 100:F2}%, Ceros {((double)ceros / numeros.Length) * 100:F2}%";
+
+                txtNumero.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un número entero entre -40 y 45");
+            }
+        }
+    
+    }
+}
